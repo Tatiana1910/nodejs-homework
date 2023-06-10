0f7104d31621fca0fec73dd75b7315753b14f26d
Tatiana Kramarenko <tanya19101996@gmail.com>
2023-06-10 20:29:23 +0300

    added mail verification service

--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -2,23 +2,29 @@ const express = require("express");
 
 const {
   register,
+  verify,
+  resendVerifyEmail,
   login,
   logout,
   currentUser,
   userUpdateSubscriprion,
   userUpdateAvatar,
 } = require("../../controllers/usersControllers");
+
 const validateBody = require("../../decorators/validateBody");
 const {
   createUserValidationSchema,
   loginValidationSchema,
   UserUpdateSubscriptionSchema,
+  userEmailSchema,
 } = require("../../schemas/usersValidationSchemas");
 const authenticate = require("../../decorators/authenticate");
 const upload = require("../../decorators/upload");
 
 const router = express.Router();
 router.post("/register", validateBody(createUserValidationSchema), register);
+router.get("/verify/:verificationToken", verify);
+router.post("/verify", validateBody(userEmailSchema), resendVerifyEmail);
 
 router.post("/login", validateBody(loginValidationSchema), login);
 
